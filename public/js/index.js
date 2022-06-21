a820b80715757feff1a24742627e2a850f919772
Slavica <54640856+Slavica-lab@users.noreply.github.com>
2022-06-21 14:56:43 +0200

    Final Login

--- public/js/index.js
+++ public/js/index.js
@@ -3,5 +3,29 @@ console.log(document.cookie);
 fetch('http://localhost:3000/userdata')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        if(data.userid){
+            let li = document.createElement("li");
+            let ul = document.querySelector('#menulist');
+            let a = document.createElement("a");
+            a.href = './logout';
+            a.textContent = 'logout';
+            li.append(a);
+            ul.append(li);
+        }else{
+            let li = document.createElement("li");
+            let ul = document.querySelector('#menulist');
+            let a = document.createElement("a");
+            a.href = './login.html';
+            a.textContent = 'login';
+            li.append(a);
+            ul.append(li);
+
+            li = document.createElement("li");
+            a = document.createElement("a");
+            a.href = './signup.html';
+            a.textContent = 'sign up';
+            li.append(a);
+            ul.append(li);
+        }
+
     });
