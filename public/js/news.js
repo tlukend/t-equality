44a64ccafe930a9ed72bc91ccd83a52db3aa1c50
Are <maksimovic34@gmail.com>
2022-06-22 15:38:04 +0200

    news fix

--- public/js/news.js
+++ public/js/news.js
@@ -1,19 +1,20 @@
 fetch('http://localhost:3000/api/news/topheadlines/')
-.then(response => response.json())
-.then(newsArray => {
+    .then(response => response.json())
+    .then(newsArray => {
 
-    if(newsArray.length === 0)
-    {const list = document.querySelector("#newslist");
-        let li = document.createElement("li");
-        li.textContent = 'There are currently no Articles';
-        list.append(li);
-     }else{
-        for (const news of Array.from(newsArray)) {
-        const list = document.querySelector("#newslist");
-        let li = document.createElement("li");
-        let h4 = document.createElement("h4");
-        h4.textContent = news.title;
-        li.append(h4.textContent+news.description);
-        list.append(li);
-    }}
-});
+        if (newsArray.length === 0) {
+            const list = document.querySelector("#newslist");
+            let p = document.createElement("p");
+            p.textContent = 'There are currently no Articles';
+            list.append(p);
+        } else {
+            for (const news of Array.from(newsArray)) {
+                const list = document.querySelector("#newslist");
+                let p = document.createElement("p");
+                let h4 = document.createElement("h4");
+                h4.textContent = news.title;
+                p.append(h4.textContent + news.description);
+                list.append(p);
+            }
+        }
+    });
