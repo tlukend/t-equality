6d60fb76a330cdba8e321c824c7e147a376f3a17
Far108 <92019144+Far108@users.noreply.github.com>
2022-06-22 22:41:56 +0200

    Games, ferig!

--- public/js/game.js
+++ public/js/game.js
@@ -1,10 +1,24 @@
 fetch('http://localhost:3000/api/games/getgames')
     .then(response => response.json())
     .then(games => {
-        for (const game of Array.from(games)) {
-            const list = document.querySelector("#gamelist");
+        const list = document.querySelector("#gameslist");
+        if (games.length === 0) {            
             let p = document.createElement("p");
-            p.append("game: " + game.name + " Alter:" + game.fsk);
+            p.textContent = 'There are currently no games';
             list.append(p);
-        }
-    });
+        } else {
+            for (const game of Array.from(games)) {                
+                let div = document.createElement("div");
+                let p = document.createElement("p");
+                let h4 = document.createElement("h4");                
+                let link = document.createElement('a');                
+                h4.textContent = game.name;
+                link.textContent = game.link;
+                p.textContent = game.beschreibung;
+                link.href = game.link;
+                link.target="_blank";
+                div.append(h4, p, link);
+                list.append(div);
+            }
+        }        
+});
